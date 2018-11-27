1a71c29565ffcc9be447847012e17876dc644b55
Ciuse <giuseppe3.mauri@mail.polimi.it>
2018-11-27 23:20:51 +0100

    Added CloudModel

---
+++ TamaDolphin/Assets/Script/HttpGet.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class HttpGet : MonoBehaviour
+{
+
+    public Text responseText;
+
+    public void Request()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://www.polimi.it/");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            responseText.text = www.downloadHandler.text;
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+    }
+}
